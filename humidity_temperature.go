3f44929faad1dab12138428f8b0e051c4901db6c
Nils Stinnesbeck <nils.stinnesbeck@unbelievable-machine.com>
2022-11-02 15:44:21 +0100

    added new functions

--- humidity_temperature.go
+++ humidity_temperature.go
@@ -7,16 +7,47 @@ import (
 	"tinygo.org/x/drivers/dht"
 )
 
-// Setup configures a DHT22 sensor on pin p and polls every t interval.
+type Device struct {
+	dht.Device
+}
+
+// Setup configures a DHT22 sensor on pin p and polls every t interval,
 // the smallest value t is allowed to take is 2 seconds
-func Setup(p machine.Pin, t time.Duration) {
+func Setup(p machine.Pin, t time.Duration) Device {
 	// set sensor type
-	sensor := dht.New(p, dht.DHT22)
+	sensor := Device{dht.New(p, dht.DHT22)}
 
 	// configure update policy
 	sensor.Configure(dht.UpdatePolicy{UpdateTime: t})
+
+	return sensor
 }
 
-func GetTemperature() {}
+// GetTemperature receives a temperature measurement and returns it
+// alongside any error that occurred
+func (d *Device) GetTemperature() (int16, error) {
+	// get measurements from bus
+	d.ReadMeasurements()
 
-func GetHumidity() {}
+	temp, err := d.Temperature()
+	return temp, err
+}
+
+// GetHumidity receives a humidity measurement and returns it
+// alongside any error that occurred
+func (d *Device) GetHumidity() (uint16, error) {
+	// get measurements from bus
+	d.ReadMeasurements()
+
+	hum, err := d.Humidity()
+	return hum, err
+}
+
+// GetAll retrieves Temperature and Humidity readings and returns it
+// alongside any error that occurred
+func (d *Device) GetAll() (int16, uint16, error) {
+	d.ReadMeasurements()
+
+	temp, hum, err := d.Measurements()
+	return temp, hum, err
+}
